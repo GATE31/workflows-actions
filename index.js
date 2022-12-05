a09629584a7a3ff9338b4df629f0654e9f9f2142
GATE31 <gate31@MacBook-Pro-web-razrabotcik.local>
2022-12-05 08:43:51 +0600

    .

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { exec } = require('node:child_process');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // docker-build
+  // docker-push
+  // update-work-version
+
+  const projectVersion = core.getInput('project-version');
+
+  exec('ls', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+  console.log(`projectVersion: ${projectVersion}!`);
+  core.setOutput("time", projectVersion);
+
+
+
+
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
